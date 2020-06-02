91238313b2484e32853bf96be347824d364a9bb4
Aleksandr3012 <flaming541@gmail.com>
2020-06-02 09:12:34 +0300

    done

--- public/js/common.js
+++ public/js/common.js
@@ -110,7 +110,7 @@ function eventHandler() {
 
 	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/Quiz3.png);"></div>')
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/form1.png);"></div>')
 	// /добавляет подложку для pixel perfect
 	// const url = document.location.href;
 	// $.each($(".top-nav__nav a "), function() {
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -118,7 +118,7 @@ function eventHandler() {
 
 	// JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
-	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/Quiz3.png);"></div>')
+	// $(".main-wrapper").after('<div class="screen" style="background-image: url(screen/form1.png);"></div>')
 	// /добавляет подложку для pixel perfect
 
 
